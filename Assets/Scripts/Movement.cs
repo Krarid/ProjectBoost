196b00527c0eb3c9d818e9c21f646efefacdd077
Javier Meléndez <jmz.krarid@outlook.com>
2022-03-20 19:13:23 -0600

    [refactoring] Rocket boost audio is only played when tapping

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -11,56 +11,69 @@ public class Movement : MonoBehaviour
         private bool rightPressed;
 
         private Rigidbody rocketRigidbody;
+        private AudioSource audioSource;
 
         [SerializeField] private float speed = 1000.0f;
         [SerializeField] private float rotationSpeed = 100.0f;
 
     #endregion
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rocketRigidbody = GetComponent<Rigidbody>();
-        rocketRigidbody.mass = 1.0f;
-    }
+    #region methods
+      // Start is called before the first frame update
+        void Start()
+        {
+            rocketRigidbody = GetComponent<Rigidbody>();
+            rocketRigidbody.mass = 1.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        ProcessThrust();
-        ProcessRotation();
-    }
+            audioSource = GetComponent<AudioSource>();
+        }
 
-    void ProcessThrust()
-    {
-        upPressed = Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow);
+        // Update is called once per frame
+        void Update()
+        {
+            ProcessThrust();
+            ProcessRotation();
+        }
 
-       if( upPressed )
-       {
-           rocketRigidbody.AddRelativeForce(Vector3.up * speed * Time.deltaTime);
-       }
-    }
+        void ProcessThrust()
+        {
+            upPressed = Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow);
 
-    void ProcessRotation()
-    {
-        leftPressed = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
-        rightPressed = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+            if( upPressed )
+            {
+                rocketRigidbody.AddRelativeForce(Vector3.up * speed * Time.deltaTime); // Boost the rocket
 
-       if( leftPressed && !rightPressed  )
-       {
-           ApplyRotation( Vector3.forward );
-       }  
-       
-       if( rightPressed && !leftPressed )
-       {
-           ApplyRotation( Vector3.back );
-       } 
-    }
+                if( !audioSource.isPlaying ) // Avoid audio playing several times in once
+                {
+                    audioSource.Play();
+                } 
+                
+            } else {
+                audioSource.Stop();
+            }
+        }
 
-    void ApplyRotation( Vector3 orientation )
-    {
-        rocketRigidbody.freezeRotation = true;
-        transform.Rotate(orientation * rotationSpeed * Time.deltaTime);
-        rocketRigidbody.freezeRotation = false;
-    }
+        void ProcessRotation()
+        {
+            leftPressed = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+            rightPressed = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+
+            if( leftPressed && !rightPressed  )
+            {
+                ApplyRotation( Vector3.forward );
+            }  
+            
+            if( rightPressed && !leftPressed )
+            {
+                ApplyRotation( Vector3.back );
+            } 
+        }
+
+        void ApplyRotation( Vector3 orientation )
+        {
+            rocketRigidbody.freezeRotation = true;
+            transform.Rotate(orientation * rotationSpeed * Time.deltaTime);
+            rocketRigidbody.freezeRotation = false;
+        }
+    #endregion
 }
